376ce36918402da7acc809bc92af2d6219524513
medousa <clement.robin09@gmail.com>
2017-12-01 11:41:17 +0100

    1/12 - debug

--- algo_largeur.ml
+++ algo_largeur.ml
@@ -1,6 +1,4 @@
-(* test *)
-
-module Set = Fifo;
+(*module Set = Fifo;*)
   
 type state =
     Free
@@ -14,23 +12,30 @@ type origin =
   | Start
   | End of int;;
 
-(* type cell =
-    None
-  | Case of case1
-and case1 =
-    {
-    walls : int array;
-    mutable state : state;
-    mutable father : cell;
-    mutable origin : cell
-   };; *)
-
 type case = {
     walls : int array;
     mutable state : state;
     mutable father : int * int;
     mutable origin : origin
    };;
+
+type 'a t = 'a list * 'a list
+
+exception Empty
+
+let empty = ([], [])
+
+let is_empty = fun q ->
+  q = empty
+
+let add = fun x (front, back) ->
+  (x::front, back)
+
+let rec take = fun q ->
+  match q with
+    ([], []) -> raise Empty
+  | (front, []) -> take ([], List.rev front)
+  | (front, x :: back) -> (x, (front, back))
 
 let rec pere_fils = fun lab (file, b) -> (* prend en parametre le labyrinthe et la file et revoie la file mise a jour *)
 	if (b = true) then
@@ -42,44 +47,64 @@ let rec pere_fils = fun lab (file, b) -> (* prend en parametre le labyrinthe et 
 		match t with
 	    	[|0; a; b; c|] -> (* test le mur du haut *)
 	      	if lab.(x-1).(y).state = Free then
-			lab.(x-1).(y).state = Marked
-		  	Set.add lab.(x-1).(y) f
-		  	lab.(x-1).(y).father = lab.(x).(y)
-		  	lab.(x-1).(y).origin = lab.(x).(y).origin
+			begin
+				lab.(x-1).(y).state = Marked;
+			  	(*Set.add lab.(x-1).(y) f*)
+				add lab.(x-1).(y) f;
+			  	lab.(x-1).(y).father = lab.(x).(y)
+			  	lab.(x-1).(y).origin = lab.(x).(y).origin
+			end
 	      	else if lab.(x-1).(y).state = Marked && lab.(x).(y).origin <> lab.(x-1).(y).origin then
-			lab.(x).(y).state = Path
-		  	lab.(x-1).(y).state = Path
-		  	b = true
+			begin
+				lab.(x).(y).state = Path
+			  	lab.(x-1).(y).state = Path
+			  	b = true
+			end
 	  	| [|a; 0; b; c|] -> (* test le mur de gauche *)
 	      	if lab.(x).(y-1).state = Free then
-			lab.(x).(y-1).state = Marked
-		  	Set.add lab.(x).(y-1) f
-		  	lab.(x).(y-1).father = lab.(x).(y)
-		  	lab.(x).(y-1).origin = lab.(x).(y).origin;
+			begin
+				lab.(x).(y-1).state = Marked;
+			  	(*Set.add lab.(x).(y-1) f*)
+				add lab.(x).(y-1) f
+			  	lab.(x).(y-1).father = lab.(x).(y)
+			  	lab.(x).(y-1).origin = lab.(x).(y).origin;
+			end
 	      	else if lab.(x).(y-1).state = Marked && lab.(x).(y).origin <> lab.(x).(y-1).origin then
-			lab.(x).(y).state = Path
-		  	lab.(x).(y-1).state = Path
-		  	b = true
+			begin
+				lab.(x).(y).state = Path
+			  	lab.(x).(y-1).state = Path
+			  	b = true
+			end
 	  	| [|a; b; 0; c|] -> (* test le mur du bas *)
 	      	if lab.(x+1).(y).state = Free then
-			lab.(x+1).(y).state = Marked
-		  	Set.add lab.(x+1).(y) f
-		  	lab.(x+1).(y).father = lab.(x).(y)
-		  	lab.(x+1).(y).origin = lab.(x).(y).origin;
+			begin
+				lab.(x+1).(y).state = Marked;
+			  	(*Set.add lab.(x+1).(y) f*)
+				add lab.(x+1).(y) f
+			  	lab.(x+1).(y).father = lab.(x).(y)
+			  	lab.(x+1).(y).origin = lab.(x).(y).origin;
+			end
 	      	else if lab.(x+1).(y).state = Marked && lab.(x).(y).origin <> lab.(x+1).(y).origin then
-			lab.(x).(y).state = Path
-		  	lab.(x+1).(y).state = Path
-		  	b = true
+			begin
+				lab.(x).(y).state = Path
+			  	lab.(x+1).(y).state = Path
+			  	b = true
+			end
 	  	| [|a; b; c; 0|] -> (* test le mur de droite *)
 	      	if lab.(x).(y+1).state = Free then
-			lab.(x).(y+1).state = Marked
-		  	Set.add lab.(x).(y+1) f
-		  	lab.(x).(y+1).father = lab.(x).(y)
-		  	lab.(x).(y+1).origin = lab.(x).(y).origin;;
+			begin
+				lab.(x).(y+1).state = Marked;
+			  	(*Set.add lab.(x).(y+1) f*)
+				add lab.(x).(y+1) f
+			  	lab.(x).(y+1).father = lab.(x).(y)
+			  	lab.(x).(y+1).origin = lab.(x).(y).origin;;
+			end
 	      	else if lab.(x).(y+1).state = Marked && lab.(x).(y).origin <> lab.(x).(y+1).origin then
-			lab.(x).(y).state = Path
-		  	lab.(x).(y+1).state = Path
-		  	b = true
+			begin
+				lab.(x).(y).state = Path
+			  	lab.(x).(y+1).state = Path
+			  	b = true
+			end
 		in
 		pere_fils lab (f, b);;
 
@@ -117,22 +142,3 @@ let maze = [|[|{walls = [|1; 1; 0; 1|]; state = Free; father = (-1, -1); origin 
     {walls = [|1; 0; 1; 0|]; state = Free; father = (-1, -1); origin = None};
     {walls = [|1; 0; 1; 0|]; state = Free; father = (-1, -1); origin = None};
     {walls = [|0; 0; 1; 1|]; state = Free; father = (-1, -1); origin = None}|]|];;
-
-
-type 'a t = 'a list * 'a list
-
-exception Empty
-
-let empty = ([], [])
-
-let is_empty = fun q ->
-  q = empty
-
-let add = fun x (front, back) ->
-  (x::front, back)
-
-let rec take = fun q ->
-  match q with
-    ([], []) -> raise Empty
-  | (front, []) -> take ([], List.rev front)
-  | (front, x :: back) -> (x, (front, back))
