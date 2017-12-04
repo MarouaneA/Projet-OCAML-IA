0ff6985ce4c9756a2e0649c0c8b31c8f0ed6a72e
medousa <clement.robin09@gmail.com>
2017-12-04 09:56:54 +0100

    4/12 - debug

--- algo_largeur.ml
+++ algo_largeur.ml
@@ -37,93 +37,83 @@ let rec take = fun q ->
   | (front, []) -> take ([], List.rev front)
   | (front, x :: back) -> (x, (front, back))
 
-let rec pere_fils = fun lab (file, b) -> (* prend en parametre le labyrinthe et la file et revoie la file mise a jour *)
+let rec pere_fils = fun lab file b -> (* prend en parametre le labyrinthe et la file et revoie la file mise a jour *)
 	if (b = true) then
 		Printf.printf "labyrinthe terminé"
 	else
+		let b = ref false in
 		let ((x, y), f) = take file in
 		(*let (c, f) = Set.take file in*)
 	 	let t = lab.(x).(y).walls in
-		match t with
-	    	[|0; a; d; c|] -> (* test le mur du haut *)
-		      	if lab.(x-1).(y).state = Free then
-				begin
-					lab.(x-1).(y).state <- Marked;
-				  	(*Set.add lab.(x-1).(y) f*)
-					add ((x-1),y) f;
-				  	lab.(x-1).(y).father <- (x, y);
-				  	lab.(x-1).(y).origin <- lab.(x).(y).origin
-				end
-		      	else if lab.(x-1).(y).state = Marked && lab.(x).(y).origin <> lab.(x-1).(y).origin then
-				begin
-					lab.(x).(y).state <- Path;
-				  	lab.(x-1).(y).state <- Path;
-				  	b = true
-				end
-	  	| [|a; 0; d; c|] -> (* test le mur de gauche *)
-		      	if lab.(x).(y-1).state = Free then
-				begin
-					lab.(x).(y-1).state <- Marked;
-				  	(*Set.add lab.(x).(y-1) f*)
-					add (x, (y-1)) f;
-				  	lab.(x).(y-1).father <- (x, y);
-				  	lab.(x).(y-1).origin <- lab.(x).(y).origin
-				end
-		      	else if lab.(x).(y-1).state = Marked && lab.(x).(y).origin <> lab.(x).(y-1).origin then
-				begin
-					lab.(x).(y).state <- Path;
-				  	lab.(x).(y-1).state <- Path;
-				  	b = true
-				end
-	  	| [|a; d; 0; c|] -> (* test le mur du bas *)
-		      	if lab.(x+1).(y).state = Free then
-				begin
-					lab.(x+1).(y).state <- Marked;
-				  	(*Set.add lab.(x+1).(y) f*)
-					add ((x+1),y) f;
-				  	lab.(x+1).(y).father <- (x, y);
-				  	lab.(x+1).(y).origin <- lab.(x).(y).origin
-				end
-		      	else if lab.(x+1).(y).state = Marked && lab.(x).(y).origin <> lab.(x+1).(y).origin then
-				begin
-					lab.(x).(y).state <- Path;
-				  	lab.(x+1).(y).state <- Path;
-				  	b = true
-				end
-	  	| [|a; d; c; 0|] -> (* test le mur de droite *)
-		      	if lab.(x).(y+1).state = Free then
-				begin
-					lab.(x).(y+1).state <- Marked;
-				  	(*Set.add lab.(x).(y+1) f*)
-					add (x,(y+1)) f;
-				  	lab.(x).(y+1).father <- (x, y);
-				  	lab.(x).(y+1).origin <- lab.(x).(y).origin
-				end
-		      	else if lab.(x).(y+1).state = Marked && lab.(x).(y).origin <> lab.(x).(y+1).origin then
-				begin
-					lab.(x).(y).state <- Path;
-				  	lab.(x).(y+1).state <- Path;
-				  	b = true
-				end
-		pere_fils lab (f, b);;
-
- 
-let () =
-	let lab = maze in 
-	let (x, y) = (2, 3) in
-	lab.(x).(y).origin = Start;
-	let (xs, ys) = (1, 1) in
-	lab.(xs).(ys).origin = End 1;
-	let f = add (x, y) empty in
-	add (xs, ys) f;
-	pere_fils lab (f, false);;
-
-  (*let f = Set.add lab.(x).(y) Set.empty in
-  Set.add lab.(xs).(ys) f;;*)
-  (* let t = false in
-  while t = false do
-    (f, t) = pere_fils lab f; *)
-
+		if t.(0) = 0 then (* test le mur du haut *)
+			begin
+			      	if lab.(x-1).(y).state = Free then
+					begin
+						lab.(x-1).(y).state <- Marked;
+					  	(*Set.add lab.(x-1).(y) f*)
+						let f = add ((x-1), y) f in
+					  	lab.(x-1).(y).father <- (x, y);
+					  	lab.(x-1).(y).origin <- lab.(x).(y).origin
+					end
+			      	else if lab.(x-1).(y).state = Marked && lab.(x).(y).origin <> lab.(x-1).(y).origin then
+					begin
+						lab.(x).(y).state <- Path;
+					  	lab.(x-1).(y).state <- Path;
+					  	b := true
+					end
+			end;
+		if t.(1) = 0 then (* test le mur de gauche *)
+			begin
+			      	if lab.(x).(y-1).state = Free then
+					begin
+						lab.(x).(y-1).state <- Marked;
+					  	(*Set.add lab.(x).(y-1) f*)
+						let f = add (x, (y-1)) f in
+					  	lab.(x).(y-1).father <- (x, y);
+					  	lab.(x).(y-1).origin <- lab.(x).(y).origin
+					end
+			      	else if lab.(x).(y-1).state = Marked && lab.(x).(y).origin <> lab.(x).(y-1).origin then
+					begin
+						lab.(x).(y).state <- Path;
+					  	lab.(x).(y-1).state <- Path;
+					  	b := true
+					end
+			end;
+		if t.(2) = 0 then (* test le mur du bas *)
+			begin
+			      	if lab.(x+1).(y).state = Free then
+					begin
+						lab.(x+1).(y).state <- Marked;
+					  	(*Set.add lab.(x+1).(y) f*)
+						let f = add ((x+1),y) f in
+					  	lab.(x+1).(y).father <- (x, y);
+					  	lab.(x+1).(y).origin <- lab.(x).(y).origin
+					end
+			      	else if lab.(x+1).(y).state = Marked && lab.(x).(y).origin <> lab.(x+1).(y).origin then
+					begin
+						lab.(x).(y).state <- Path;
+					  	lab.(x+1).(y).state <- Path;
+					  	b := true
+					end
+			end;
+		if t.(3) = 0 then (* test le mur de droite *)
+			begin
+			      	if lab.(x).(y+1).state = Free then
+					begin
+						lab.(x).(y+1).state <- Marked;
+					  	(*Set.add lab.(x).(y+1) f*)
+						let f = add (x,(y+1)) f in
+					  	lab.(x).(y+1).father <- (x, y);
+					  	lab.(x).(y+1).origin <- lab.(x).(y).origin
+					end
+			      	else if lab.(x).(y+1).state = Marked && lab.(x).(y).origin <> lab.(x).(y+1).origin then
+					begin
+						lab.(x).(y).state <- Path;
+					  	lab.(x).(y+1).state <- Path;
+					  	b := true
+					end
+			end;
+		pere_fils lab f !b;;
 
 let maze = [|[|{walls = [|1; 1; 0; 1|]; state = Free; father = (-1, -1); origin = None};
     {walls = [|1; 1; 0; 1|]; state = Free; father = (-1, -1); origin = None};
@@ -141,3 +131,19 @@ let maze = [|[|{walls = [|1; 1; 0; 1|]; state = Free; father = (-1, -1); origin 
     {walls = [|1; 0; 1; 0|]; state = Free; father = (-1, -1); origin = None};
     {walls = [|1; 0; 1; 0|]; state = Free; father = (-1, -1); origin = None};
     {walls = [|0; 0; 1; 1|]; state = Free; father = (-1, -1); origin = None}|]|];;
+
+let () =
+	let lab = maze in 
+	let (x, y) = (2, 3) in
+	lab.(x).(y).origin <- Start;
+	let (xs, ys) = (1, 1) in
+	lab.(xs).(ys).origin <- End 1;
+	let f = add (x, y) empty in
+	let f = add (xs, ys) f in
+	pere_fils lab f false;;
+
+  (*let f = Set.add lab.(x).(y) Set.empty in
+  Set.add lab.(xs).(ys) f;;*)
+  (* let t = false in
+  while t = false do
+    (f, t) = pere_fils lab f; *)
